794b86aeb9b570486e1f11d83c2d36553b9fc09b
Votre Nom <Vous@exemple.com>
2023-03-23 09:50:03 +0100

    ajout language + modif couleur + ajout bouton aide sur QuizzActivity

--- app/src/main/java/fr/sio/openstreetmaptest/QuizzActivity.java
+++ app/src/main/java/fr/sio/openstreetmaptest/QuizzActivity.java
@@ -2,22 +2,27 @@ package fr.sio.openstreetmaptest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizzActivity extends AppCompatActivity implements View.OnClickListener{
     Button bValider;
     Button bRetour;
+    Button bAide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
         bValider = findViewById(R.id.Valider1);
         bRetour = findViewById(R.id.Retour);
+        bAide = findViewById(R.id.aide);
         bValider.setOnClickListener(this);
         bRetour.setOnClickListener(this);
+        bAide.setOnClickListener(this);
     }
 
     @Override
@@ -25,6 +30,12 @@ public class QuizzActivity extends AppCompatActivity implements View.OnClickList
         if (v == bValider){
             Intent i = new Intent(this, reponseActivity.class);
             startActivity(i);
+        }
+        else if (v == bAide){
+            Context context = getApplicationContext();
+            String mess = "Aide";
+            int duration = Toast.LENGTH_LONG;
+            Toast.makeText(context, mess, duration).show();
         }
         else if (v == bRetour){ finish(); }
     }
